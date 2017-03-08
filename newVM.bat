1faff8109b3194756e427d2a1a812c6d0bb5b8e3
kform8 <kform8@ads.intra.inist.fr>
2017-03-08 23:07:18 +0100

    Utilisation de la variable VBOX_MSI_INSTALL_PATH qui est définie à l'installation de virtualbox

--- newVM.bat
+++ newVM.bat
@@ -1,27 +1,26 @@
-set VBOXPATH=C:\Program Files\Oracle\VirtualBox
 set VMPATH=d:\Virtualbox
 
-"%VBOXPATH%\VboxManage.exe" list vms > vms.txt
+"%VBOX_MSI_INSTALL_PATH%\VboxManage.exe" list vms > vms.txt
 FOR /F delims^=^"^ tokens^=1* %%m in (vms.txt) do (
 	IF /I %%m==V%COMPUTERNAME% ( 
 		rem suppression de la machine virtuelle
-                "%VBOXPATH%\VboxManage.exe" unregistervm "V%COMPUTERNAME%" --delete
+                "%VBOX_MSI_INSTALL_PATH%\VboxManage.exe" unregistervm "V%COMPUTERNAME%" --delete
 		rem Suppression du disque dur virtuel associé à la machine
-		rem "%VBOXPATH%\VboxManage.exe" closemedium "%VMPATH%\V%COMPUTERNAME%\V%COMPUTERNAME%.vdi" --delete				
+		rem "%VBOX_MSI_INSTALL_PATH%\VboxManage.exe" closemedium "%VMPATH%\V%COMPUTERNAME%\V%COMPUTERNAME%.vdi" --delete				
 	)
 
 )
 
-"%VBOXPATH%\VboxManage.exe" createvm --name "V%COMPUTERNAME%" --ostype Windows7_64
-"%VBOXPATH%\VboxManage.exe" registervm "%VMPATH%\V%COMPUTERNAME%\V%COMPUTERNAME%.vbox"
+"%VBOX_MSI_INSTALL_PATH%\VboxManage.exe" createvm --name "V%COMPUTERNAME%" --ostype Windows7_64
+"%VBOX_MSI_INSTALL_PATH%\VboxManage.exe" registervm "%VMPATH%\V%COMPUTERNAME%\V%COMPUTERNAME%.vbox"
 
 for /F %%M in ('powershell -ExecutionPolicy Bypass -NoProfile -File .\getmac.ps1') do (
 	set MACADDRESS=%%M
 )
 
-"%VBOXPATH%\VboxManage.exe" modifyvm  "V%COMPUTERNAME%"  --memory 4096 --vram 32 --acpi on --apic on --paravirtprovider hyperv --cpus 2 --firmware bios --boot1 net --boot2 disk --boot3 dvd --nic1 bridged --nictype1 82540EM --cableconnected1 on --macaddress1 %MACADDRESS% --clipboard bidirectional --draganddrop bidirectional
-"%VBOXPATH%\VboxManage.exe" createmedium disk --filename "%VMPATH%\V%COMPUTERNAME%\V%COMPUTERNAME%.vdi" --format vdi --size 256000 --variant standard
-"%VBOXPATH%\VboxManage.exe" storagectl "V%COMPUTERNAME%" --name "SATA" --add sata --controller IntelAHCI --portcount 4 --hostiocache on --bootable on
-"%VBOXPATH%\VboxManage.exe" storageattach "V%COMPUTERNAME%" --storagectl "SATA" --port 0 --device 0 --type hdd --medium "%VMPATH%\V%COMPUTERNAME%\V%COMPUTERNAME%.vdi"
-"%VBOXPATH%\VboxManage.exe" storageattach "V%COMPUTERNAME%" --storagectl "SATA" --port 1 --device 0 --type dvddrive --medium emptydrive
+"%VBOX_MSI_INSTALL_PATH%\VboxManage.exe" modifyvm  "V%COMPUTERNAME%"  --memory 4096 --vram 32 --acpi on --apic on --paravirtprovider hyperv --cpus 2 --firmware bios --boot1 net --boot2 disk --boot3 dvd --nic1 bridged --nictype1 82540EM --cableconnected1 on --macaddress1 %MACADDRESS% --clipboard bidirectional --draganddrop bidirectional
+"%VBOX_MSI_INSTALL_PATH%\VboxManage.exe" createmedium disk --filename "%VMPATH%\V%COMPUTERNAME%\V%COMPUTERNAME%.vdi" --format vdi --size 256000 --variant standard
+"%VBOX_MSI_INSTALL_PATH%\VboxManage.exe" storagectl "V%COMPUTERNAME%" --name "SATA" --add sata --controller IntelAHCI --portcount 4 --hostiocache on --bootable on
+"%VBOX_MSI_INSTALL_PATH%\VboxManage.exe" storageattach "V%COMPUTERNAME%" --storagectl "SATA" --port 0 --device 0 --type hdd --medium "%VMPATH%\V%COMPUTERNAME%\V%COMPUTERNAME%.vdi"
+"%VBOX_MSI_INSTALL_PATH%\VboxManage.exe" storageattach "V%COMPUTERNAME%" --storagectl "SATA" --port 1 --device 0 --type dvddrive --medium emptydrive
 exit %ERRORLEVEL%
